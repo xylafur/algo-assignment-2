733d097aba8f768e7e8f7c79efd201fa19411632
Raymond Sutrisno <raymond@sutrisno.me>
2018-04-01 20:08:22 -0500

    huffman

--- prog5/huffman.py
+++ prog5/huffman.py
@@ -1,93 +1,78 @@
-from heap import *
 
-def get_character_count(str):
-    """
-        Returns the count of all characters in a string
+class HuffmanNode:
+   def __init__(self, symbols, freq):
+      self.symbols = symbols
+      self.freq = freq
+      self.left = None
+      self.right = None
 
-        Arguments:
-            str (:class:`str`): The input string
+   def is_leaf(self):
+      return (self.left is None) and (self.right is None)
 
-        Returns:
-            (:class:`dict`): Each key is a character, each value its appearance
-                             count.  The key total coresponds to the total number
-                             of characters in the string.
+   def __lt__(self, other):
+      return (self.freq, self.symbols) < (other.freq, other.symbols)
+   def __le__(self, other):
+      return (self.freq, self.symbols) <= (other.freq, other.symbols)
+   def __gt__(self, other):
+      return (self.freq, self.symbols) > (other.freq, other.symbols)
+   def __ge__(self, other): return (self.freq, self.symbols) >= (other.freq, other.symbols)
 
-        Complexity:
-            O(n), where n is the length of the string
-    """
-    count = {'total': 0}
-    for char in str:
-        if char in count.keys():
-            count[char] += 1
-        else:
-            count[char] = 1
-        count['total'] += 1
-    return count
+   def __str__(self):
+      S = '({},{})'.format(self.symbols, self.freq)
+      if self.left:
+         S = self.left.symbols + '|' + S
+      if self.right:
+         S = S + '|' + self.right.symbols
+      return '[{}]'.format(S)
 
-def frequency_from_count(count):
-    """
-        Returns the frequency of all characters in a string
+   def __repr__(self):
+      return '"{}"'.format(self.__str__())
 
-        Arguments:
-            count (:class:`dict`): The count of characters from a string
 
-        Returns:
-            (:class:`dict`): The frequency as a dictionary
+def ParentHuffman(L, R):
+   H = HuffmanNode('{}{}'.format(L.symbols,R.symbols), L.freq + R.freq)
+   H.left, H.right = L, R
+   return H
 
-        Complexity:
-            O(n), where n is the length of the string
-    """
-    return {char: count[char] / count['total'] for char in count.keys() \
-            if char != 'total'}
+def Huffman(data):
+   # record frequency
+   freqs = {}
+   for i in data:
+      if i not in freqs: freqs[i] = 1
+      else: freqs[i] += 1
 
-class HuffmanTree:
-    def __init__(self, frequency):
-        chars = [HufChar(char, freq) for char, freq in frequency.items()]
-        self.heap = HufMinHeap()
-        for char in chars:
-            self.heap.add_element(char)
+   # create heap
+   import heapq
+   heap = [HuffmanNode(s, freqs[s]) for s in freqs]
+   heapq.heapify(heap)
 
-        print(self.heap.height)
+   while len(heap) > 1:
+      heapq.heappush(heap,
+         ParentHuffman(heapq.heappop(heap), heapq.heappop(heap)))
 
-        while self.heap.height > 1:
-            left, right = self.heap.pop_min(), self.heap.pop_min()
-            new = HufChar(left.char + right.char, left.freq + right.freq)
-            new.left = left
-            new.right = right
-            self.heap.add_element(new)
+   root = heap[0]
+   codes = {}
 
-    def get_codes(self):
-        def _get_codes(codes, node, code):
-            if node is None:
-                return
-            if node.left is None and node.right is None:
-                codes[node.char] = code
-            _get_codes(codes, node.left, code + "0")
-            _get_codes(codes, node.right, code + "1")
+   def recurse(node, code):
+      if node.is_leaf():
+         codes[node.symbols] = code
+         return
 
-        codes = {}
-        _get_codes(codes, self.heap.pop_min(), "")
-        return codes
+      if node.left:
+         recurse(node.left, code+'0')
+      if node.right:
+         recurse(node.right, code+'1')
 
-def string_to_huffman(str):
-    """
-        This function takes in a string of arbitrary length and returns an 
-        optimal huffman code for it.
+   recurse(root, '')
+   return heap[0], codes, freqs
 
-        Arguments:
-            str (:class:`str`): The input string
+def Main():
+   Data = input("Enter some data: ")
+   print('You entered: ', Data)
+   root, codes, freqs = Huffman(Data)
+   print('Frequencies: ', freqs)
+   print('Codes: ', codes)
 
-        Returns:
-            (:class:`str`): The string representation of a binary huffman code
+if __name__=='__main__':
+   Main()
 
-        Complexity:
-            O(nlogn)
-    """
-    count = get_character_count(str)
-    freq = frequency_from_count(count)
-    huff = HuffmanTree(freq)
-    return huff.get_codes()
-
-if __name__ == '__main__':
-    string = input("enter a string: ")
-    print(string_to_huffman(string))
